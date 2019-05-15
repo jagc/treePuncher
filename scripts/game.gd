d0273834e7dfff34306b34e179f05907ddc99332
jagc <jag.cordoviz@gmail.com>
2019-05-16 06:08:28 +0800

    set trunk sprites in the game

--- scripts/game.gd
+++ scripts/game.gd
@@ -4,10 +4,19 @@ export(PackedScene) var trunk_scene
 
 onready var first_trunk_position = $firstTrunkPosition
 
-func _ready():
+var last_spawn_position
+
+func _ready():	
+	last_spawn_position = first_trunk_position.position
 	_spawn_first_trunks()
-	
+
+func _process(delta):
+	if Input.is_action_just_pressed("close"):
+		get_tree().quit()
+
 func _spawn_first_trunks():
-	var new_trunk = trunk_scene.instance()
-	add_child(new_trunk)
-	new_trunk.position = first_trunk_position.position
+	for counter in range(5):
+		var new_trunk = trunk_scene.instance()
+		add_child(new_trunk)
+		new_trunk.position = last_spawn_position
+		last_spawn_position.y -= new_trunk.sprite_height
---
+++ scripts/trunk.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+onready var sprite = $Sprite
+var sprite_height
+
+func _ready():
+	sprite_height = sprite.texture.get_height() * scale.y
