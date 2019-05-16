287624f5c5a6a1a714953f9f72d9ed6710a71880
jagc <jag.cordoviz@gmail.com>
2019-05-16 10:24:30 +0800

    created scene logic for trunk and it's axe

--- scripts/trunk.gd
+++ scripts/trunk.gd
@@ -1,7 +1,20 @@
 extends Node2D
 
 onready var sprite = $Sprite
+onready var left_axe = $leftAxe
+onready var right_axe = $rightAxe
+
 var sprite_height
 
 func _ready():
 	sprite_height = sprite.texture.get_height() * scale.y
+	
+func initialize_trunk(axe, right):
+	if axe:
+		if right:
+			left_axe.queue_free()
+		else:
+			right_axe.queue_free()
+	else:
+		left_axe.queue_free()
+		right_axe.queue_free()
